b09df3b9c0e426c6a9aeeda05ee4f9b4da79309e
Mike <37367675+chinarev@users.noreply.github.com>
2020-11-22 20:50:37 +0300

    add new login

--- src/components/main-page/Body.js
+++ src/components/main-page/Body.js
@@ -1,11 +1,26 @@
 import React from "react";
 import pic from '../../assets/img/spotify_logo.png'
+import {authEndpoint, clientId, redirectUri, scopes} from "../../service/config.js";
+import hash from "../../service/hash.js";
 
 class Body extends React.Component {
     onclick() {
-        window.location.assign('http://localhost:8888');
+        window.location.assign(`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`);
     }
 
+    componentDidMount() {
+        // Set token
+        let _token = hash.access_token;
+
+        if (_token) {
+            // Set token
+            this.setState({
+                token: _token
+            });
+
+        }
+    }
+//href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
     render() {
         return <div id="main-container">
             <h1 className="main-page">Welcome to SPALCO</h1>
@@ -15,8 +30,7 @@ class Body extends React.Component {
                 <div className="fields container-element">
                     <input type="email" placeholder="Email" className="main-page"/>
                     <input type="password" placeholder="Password" className="main-page"/>
-                </div>
-                <button className="main-page container-element" onClick={(e) => this.onclick(e)}>Log in</button>
+                </div>                <button className="main-page container-element" onClick={(e) => this.onclick(e)}>Log in</button>
             </div>
         </div>;
     }
