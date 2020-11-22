162b49fe8c4349ed8b871cbda84481782dd8c34c
01gaM <olga.molchanova.olkamana@gmail.com>
2020-11-23 01:46:38 +0300

    changing name

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -1,17 +1,30 @@
 import React from "react";
-import Body from "./Body";
 import '../../assets/mainPageStyle.css'
+import pic from "../../assets/img/spotify_logo.png";
+import {authEndpoint, clientId, redirectUri, scopes} from "../../service/config.js";
 
 
 class MainPage extends React.Component {
+
+    onclick() {
+        window.location.assign(`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`);
+    }
+
     componentDidMount() {
         document.title = 'SPALCO - Main page';
     }
 
     render() {
         return <div className="main-page">
-            <Body/>
-        </div>
+            <div id="main-container">
+                <h1 className="main-page">Welcome to SPALCO</h1>
+                <h1 className="main-page">Spotify album cover editor</h1>
+                <div id="auth-container">
+                    <img src={pic} id="logo" className="container-element" alt="Spotify logo"/>
+                    <button className="main-page container-element" onClick={(e) => this.onclick(e)}>Log in</button>
+                </div>
+            </div>
+        </div>;
     }
 }
 
--- src/service/config.js
+++ src/service/config.js
@@ -1,10 +1,10 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-
-// Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "8de10b0811644024ac6db180ef2bdd9d";
 export const redirectUri = "http://localhost:3000/second";
 export const scopes = [
     "user-top-read",
-    "user-read-currently-playing",
-    "user-read-playback-state",
+    "ugc-image-upload",
+    "playlist-modify-public",
+    "playlist-modify-private",
+    "playlist-read-private"
 ];
