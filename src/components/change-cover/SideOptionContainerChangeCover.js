a8e2c240141ccf589adae5ada096729e64b81bf7
01gaM <olga.molchanova.olkamana@gmail.com>
2020-12-06 21:26:11 +0300

    Added side options navigation

--- src/components/change-cover/SideOptionContainerChangeCover.js
+++ src/components/change-cover/SideOptionContainerChangeCover.js
@@ -1,18 +1,44 @@
 import React from "react";
+import {PAGE_STATE} from "../selected-playlist-page/SideOptionsContainer";
+import SideOptionsContainer from "../selected-playlist-page/SideOptionsContainer";
+
 
 class SideOptionContainerChangeCover extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curr_page_state: PAGE_STATE.CHANGE_COVER
+        };
+    }
+
+    onclickBack() {
+        localStorage.setItem("page_state", PAGE_STATE.SELECTED_PLAYLIST);
+        this.setState({
+            curr_page_state: PAGE_STATE.SELECTED_PLAYLIST
+        });
+        console.log("state in onclickBack: " + localStorage.getItem("page_state"))
+    }
+
     render() {
-        return <div id="side-options-container">
-            <button className="side-options">
-                Choose style template
-            </button>
-            <button className="side-options">
-                Create custom style
-            </button>
-            <button className="side-options">
-                Remove cover
-            </button>
-        </div>
+        console.log("state in change cover render: " + localStorage.getItem("page_state"))
+        if (localStorage.getItem("page_state") === PAGE_STATE.SELECTED_PLAYLIST) {
+            return <SideOptionsContainer/>
+        } else {
+            return <div id="side-options-container">
+                <button className="side-options">
+                    Choose style template
+                </button>
+                <button className="side-options">
+                    Create custom style
+                </button>
+                <button className="side-options">
+                    Remove cover
+                </button>
+                <button className="side-options" onClick={(e) => this.onclickBack(e)}>
+                    Back
+                </button>
+            </div>
+        }
     }
 }
 
--- src/components/selected-playlist-page/SideOptionsContainer.js
+++ src/components/selected-playlist-page/SideOptionsContainer.js
@@ -1,24 +1,53 @@
 import React from "react";
+import SideOptionContainerChangeCover from "../change-cover/SideOptionContainerChangeCover";
+
+export const PAGE_STATE = {
+    SELECTED_PLAYLIST : "selected_playlist",
+    CHANGE_COVER : "change_cover",
+    CREATE_CUSTOM_STYLE : "create_custom_style",
+    SET_BACKGROUND_COLOR : "set_background_color"
+};
+
 
 class SideOptionsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curr_page_state: PAGE_STATE.SELECTED_PLAYLIST
+        };
+        localStorage.setItem("page_state", PAGE_STATE.SELECTED_PLAYLIST);
+    }
 
-    onclickCover() {
-        window.location.assign('http://localhost:3000/changeCover/');
+    onclickChangeCover() {
+        this.setState({
+            curr_page_state: PAGE_STATE.CHANGE_COVER
+        });
+        localStorage.setItem("page_state", PAGE_STATE.CHANGE_COVER);
+        console.log("onclickChangeCover state:" + localStorage.getItem("page_state"));
     }
 
     onclickName() {
 
     }
 
+
     render() {
-        return <div id="side-options-container">
-            <button className="side-options" onClick={(e) => this.onclickName(e)}>
-                Change name
-            </button>
-            <button onClick={(e) => this.onclickCover(e)} className="side-options" >
-                Change cover
-            </button>
-        </div>
+        console.log("state in render: " + localStorage.getItem("page_state"))
+        switch (localStorage.getItem("page_state")){
+            case (PAGE_STATE.CHANGE_COVER): {
+                return <SideOptionContainerChangeCover/>
+            }
+            default: {
+                return <div id="side-options-container">
+                    <button className="side-options" onClick={(e) => this.onclickName(e)}>
+                        Change name
+                    </button>
+                    <button onClick={(e) => this.onclickChangeCover(e)} className="side-options" >
+                        Change cover
+                    </button>
+                </div>
+            }
+        }
     }
 }
 
