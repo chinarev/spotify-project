12f245ca9b0f72cdfeaa1a4893472c9b62442d58
Mike <37367675+chinarev@users.noreply.github.com>
2020-12-07 16:33:31 +0300

    add Cover Upload

--- src/components/side-options/SideOptionContainerChangeCover.js
+++ src/components/side-options/SideOptionContainerChangeCover.js
@@ -2,8 +2,35 @@ import React from "react";
 import {PAGE_STATE} from "../selected-playlist-page/SideOptionsContainer";
 import SideOptionsContainer from "../selected-playlist-page/SideOptionsContainer";
 import SideOptionsCreateCustomStyle from "./SideOptionsCreateCustomStyle";
+import pic from '../../assets/img/morga.jpg'
+import SpotifyWebApi from "spotify-web-api-js";
 
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(localStorage.getItem("textToken"));
 
+function getBase64Image(src, callback, outputFormat) {
+    const img = new Image();
+    img.height = 260;
+    img.width = 260;
+    img.crossOrigin = 'Anonymous';
+    img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        let dataURL;
+        canvas.height = 260; //Можно сделать 560, тогда фон будет большим, но картинка все еще 260 на 260
+        canvas.width = 260;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+
+    };
+
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+        img.src = "data:image/jpeg;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+    }
+}
 class SideOptionContainerChangeCover extends React.Component {
     constructor(props) {
         super(props);
@@ -27,10 +54,28 @@ class SideOptionContainerChangeCover extends React.Component {
         });
     }
 
-    onClickChooseStyleTemplate(){
-        window.location.assign(`http://localhost:3000/style_gallery`);
-    }
+    onclickUploadCover(){
+        var curr_img = localStorage.getItem("selected_playlist_image")
+        var playlist_id = localStorage.getItem("selected_playlist_id")
+        var new_img;
 
+        getBase64Image(
+            pic,
+            function(dataUrl) {
+                //console.log('RESULT:', dataUrl)
+                localStorage.setItem("new_playlist_cover", dataUrl);
+            }
+        );
+
+        new_img = localStorage.getItem("new_playlist_cover");
+        localStorage.setItem("selected_playlist_cover", new_img);
+        //console.log(new_img);
+        spotifyApi.uploadCustomPlaylistCoverImage(
+            playlist_id,
+            new_img.substring(new_img.indexOf(",") + 1)
+        ).then(r => console.log("r: " + r))
+        console.log(localStorage.getItem("selected_playlist_cover"));
+    }
 
     render() {
         switch (localStorage.getItem("page_state")) {
@@ -42,7 +87,10 @@ class SideOptionContainerChangeCover extends React.Component {
             }
             default: {
                 return <div id="side-options-container">
-                    <button className="side-options" onClick={(e) => this.onClickChooseStyleTemplate(e)}>
+                    <button className="side-options" onClick={(e) => this.onclickUploadCover(e)}>
+                        Upload cover
+                    </button>
+                    <button className="side-options">
                         Choose style template
                     </button>
                     <button className="side-options" onClick={(e) => this.onClickCreateCustomStyle(e)}>
