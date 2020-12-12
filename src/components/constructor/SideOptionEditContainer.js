2637e001cc7a333fae7378c22857c17a081e91ee
Mike <37367675+chinarev@users.noreply.github.com>
2020-12-12 20:53:39 +0300

    add color

--- src/components/constructor/SideOptionEditContainer.js
+++ src/components/constructor/SideOptionEditContainer.js
@@ -2,13 +2,20 @@ import React from "react";
 import SetColorForm from "../text-edit-model/SetColorForm";
 import SetTextFont from "../text-edit-model/SetTextFont";
 import SetTextSize from "../text-edit-model/SetTextSize";
-
+import { ChromePicker } from 'react-color';
 
 class SideOptionEditContainer extends React.Component {
-
+    state = {
+        background: localStorage.getItem("preview_text_color")
+    };
     onclickGallery() {
         window.location.assign(`http://localhost:3000/background_gallery/`);
     }
+    handleChangeComplete = (color) => {
+        this.setState({ background: color.hex });
+        localStorage.setItem("preview_text_color", color.hex);
+        document.location.reload();
+    };
 
     render() {
         return <div id="side-options-container">
@@ -16,7 +23,8 @@ class SideOptionEditContainer extends React.Component {
             <button className="side-options" onClick={(e) => this.onclickGallery(e)}> Choose from gallery </button>
             <button className="side-options">Upload background</button>
             <div>
-                <SetColorForm/>
+                <ChromePicker color={ this.state.background }
+                              onChangeComplete={ this.handleChangeComplete }/>
             </div>
             <div>
                 <SetTextFont/>
@@ -24,6 +32,7 @@ class SideOptionEditContainer extends React.Component {
             <div>
                 <SetTextSize/>
             </div>
+
             <button className="side-options">Save</button>
             <button className="side-options">Back</button>
         </div>;
