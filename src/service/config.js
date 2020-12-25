80bdeaa28e05fcc86c3302da24dd4af01753de4b
Mike <37367675+chinarev@users.noreply.github.com>
2020-12-25 22:09:25 +0300

    fix config

--- src/service/config.js
+++ src/service/config.js
@@ -2,7 +2,6 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "8de10b0811644024ac6db180ef2bdd9d";
 export const redirectUri = "http://localhost:3000/second";
 export const scopes = [
-    "user-top-read",
     "ugc-image-upload",
     "playlist-modify-public",
     "playlist-modify-private",
